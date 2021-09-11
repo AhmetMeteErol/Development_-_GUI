1d83ea8ff49d8dfe3e8000f254d58cb23dfe7f80
Ahmet Mete Erol <72703850+AhmetMeteErol@users.noreply.github.com>
2021-09-11 14:46:30 +0300

    Update main.py
    optimizations and developments

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ This is the backhand function for Graphical User Interface
     - With the 2nd function, this function aims to alert user if the product is under limit
 For these purposes it's defined in class format.
 '''
+
 # We need to import datetime to know current time
 from datetime import datetime, timedelta
 
@@ -20,12 +21,13 @@ from datetime import datetime, timedelta
 class Product(object):
     '''This class represents the products which had been purchased and been stored from Yaltes.'''
 
-    def __init__(self, model_id, arrivel_Time):
-        self.model_id = model_id
+    # initial function
+    def __init__(self, quantity, arrivel_Time):
+        self.quantity = quantity
         self.arrival_time = arrivel_Time
 
     def UnnecessaryStorage(self):
-
+        """Takes string values from his class and returns a boolean value if there is a situation for unnecessary storage for a product """
         delta_day = datetime.today() - \
             datetime.strptime(self.arrival_time, '%d/%m/%Y')
 
@@ -37,32 +39,36 @@ class Product(object):
         else:
             return False
 
-    def Supplied2Storage(self, model_id_added):
-        # we add the model_id so the all model_id augments
-        self.model_id = model_id_added + self.model_id
+    def Supplied2Storage(self, quantity_added):
+        """This module serves to change our product quantity."""
+        # we add the quantity so the all quantity augments
+        self.quantity = quantity_added + self.quantity
         print(
-            f"Old model_id: {self.model_id - model_id_added}, new model_id: {self.model_id}.")
+            f"Old quantity: {self.quantity - quantity_added}, new quantity: {self.quantity}.")
 
-    def Supplied2Montage(self, model_id_minued):
-        # we remove the model_id so the all model_id diminishes
-        self.model_id = self.model_id - model_id_minued
-        print(f"{model_id_minued} piece had been transferred to montage.")
+    def Supplied2Montage(self, quantity_minued):
+        """This module serves to change our product quantity."""
+
+        # we remove the quantity so the all quantity diminishes
+        self.quantity = self.quantity - quantity_minued
+        print(f"{quantity_minued} piece had been transferred to montage.")
         print(
-            f"Old model_id at warehouse: {self.model_id + model_id_minued}, new model_id at warehouse: {self.model_id}.")
+            f"Old quantity at warehouse: {self.quantity + quantity_minued}, new quantity at warehouse: {self.quantity}.")
 
 
 class KVM(Product):
-    'This class is unique to KVMs'
+    'This a product class which is unique to KVMs'
 
     # This init function is needed to inheritance the main class
-    def __init__(self, model_id, arrivel_Time):
-        Product.__init__(self, model_id, arrivel_Time)
+    def __init__(self, quantity, arrivel_Time):
+        Product.__init__(self, quantity, arrivel_Time)
 
+# As each product's limit is different, we need to redefine modules.
     def IsUnderLimit(self):
-        # It's needless to define the limit inside of function
+        """Returns a boolean value if the quantity """
         limit = 10
 
-        if self.model_id < limit:               # This function returns True to check limit situation
+        if self.quantity < limit:
             return True
         else:
             return False
@@ -71,14 +77,14 @@ class KVM(Product):
 class Workstation(Product):
     'This class is unique to Workstation'
 
-    def __init__(self, model_id, arrivel_Time):
-        Product.__init__(self, model_id, arrivel_Time)
+    def __init__(self, quantity, arrivel_Time):
+        Product.__init__(self, quantity, arrivel_Time)
 
     def IsUnderLimit(self):
         # It's needless to define the limit inside of function
         limit = 20
 
-        if self.model_id < limit:               # This function returns True to check limit situation
+        if self.quantity < limit:
             return True
         else:
             return False
@@ -87,14 +93,14 @@ class Workstation(Product):
 class LCD_32_Display(Product):
     'This class is unique to 32"LCD Display'
 
-    def __init__(self, model_id, arrivel_Time):
-        Product.__init__(self, model_id, arrivel_Time)
+    def __init__(self, quantity, arrivel_Time):
+        Product.__init__(self, quantity, arrivel_Time)
 
     def IsUnderLimit(self):
         # It's needless to define the limit inside of function
         limit = 10
 
-        if self.model_id < limit:               # This function returns True to check limit situation
+        if self.quantity < limit:
             return True
         else:
             return False
@@ -103,44 +109,14 @@ class LCD_32_Display(Product):
 class Filter(Product):
     'This class is unique to filters'
 
-    def __init__(self, model_id, arrivel_Time):
-        Product.__init__(self, model_id, arrivel_Time)
+    def __init__(self, quantity, arrivel_Time):
+        Product.__init__(self, quantity, arrivel_Time)
 
     def IsUnderLimit(self):
         # It's needless to define the limit inside of function
         limit = 100
 
-        if self.model_id < limit:               # This function returns True to check limit situation
+        if self.quantity < limit:
             return True
         else:
             return False
-
-
-class Limit_checkers():
-    '''This class aims to gather all checker functions'''
-    def KVM_checker(KVM_name):
-        if KVM_name.IsUnderLimit():
-            return True
-        else:
-            return False
-
-    def Display_checker(Display_name):
-        if Display_name.IsUnderLimit():
-            return True
-        else:
-            return False
-
-    def Filter_checker(Display_name):
-        if Display_name.IsUnderLimit():
-            return True
-        else:
-            return False
-
-    def Workstation_checker(WS_name):
-        if WS_name.IsUnderLimit():
-            return True
-        else:
-            return False
-
-
-# main içine main loop oluştur
